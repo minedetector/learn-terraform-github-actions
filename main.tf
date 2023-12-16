8f877b812b0ff9c5921fc707e80eeb6ab1bfe85a
minedetector <larsasi.la@hotmail.com>
2023-12-16 21:00:19 +0200

    wtf

--- main.tf
+++ main.tf
@@ -52,6 +52,21 @@ resource "aws_instance" "web" {
               EOF
 }
 
+resource "aws_instance" "web2" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update
+              apt-get install -y apache2
+              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
+              echo "Hello World wtf" > /var/www/html/index.html
+              systemctl restart apache2
+              EOF
+}
+
 resource "aws_security_group" "web-sg" {
   name = "${random_pet.sg.id}-sg"
   ingress {
